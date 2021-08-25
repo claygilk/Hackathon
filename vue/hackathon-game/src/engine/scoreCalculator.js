bbe256a705c1199a52018f7016f147cb96ef8c72
claygilk <claygilkerson@gmail.com>
2021-08-25 13:04:37 -0400

    added word score calculator

--- vue/hackathon-game/src/engine/scoreCalculator.js
+++ vue/hackathon-game/src/engine/scoreCalculator.js
@@ -1,17 +1,51 @@
+const scores = new Map()
+
+scores.set('A', 1)
+scores.set('E', 1)
+scores.set('I', 1)
+scores.set('O', 1)
+scores.set('U', 1)
+scores.set('L', 1)
+scores.set('N', 1)
+scores.set('S', 1)
+scores.set('T', 1)
+scores.set('R', 1)
+
+scores.set('D', 2)
+scores.set('G', 2)
+
+scores.set('B', 3)
+scores.set('C', 3)
+scores.set('M', 3)
+scores.set('P', 3)
+
+scores.set('F', 4)
+scores.set('H', 4)
+scores.set('V', 4)
+scores.set('W', 4)
+scores.set('Y', 4)
+
+scores.set('K', 5)
+
+scores.set('J', 8)
+scores.set('X', 8)
+
+scores.set('Q', 10)
+scores.set('Z', 10)
 
 
-    
-    const calcWordScore = (letterArray) => {
-    
-        let total
-    
-        letterArray.forEach(letter => {
-            total++
-        })
-    
-        return total
-    }
+const calcWordScore = (letterArray) => {
 
-    module.exports = {
-        calcWordScore
-    }
+    let total = 0
+
+    letterArray.forEach(letter => {
+        points = scores.get(letter)
+        total += points
+    })
+
+    return total
+}
+
+module.exports = {
+    calcWordScore
+}
--- vue/hackathon-game/test/score_tests.js
+++ vue/hackathon-game/test/score_tests.js
@@ -4,13 +4,22 @@ const calc = require('../src/engine/scoreCalculator')
 
 describe('Calulates point totals correctly', () => {
 
-    it('Calculates point total of word correctly', done => {
+    it('DOG is worth 5 points', done => {
 
         let letterArray = ['D', 'O', 'G']
 
         let actual = calc.calcWordScore(letterArray)
 
         assert(actual === 5)
+        done()
+    })
 
+    it('THE is worth 6 points', done => {
+        let letterArray = ['T', 'H', 'E']
+
+        let actual = calc.calcWordScore(letterArray)
+
+        assert(actual === 6)
+        done()
     })
 })
