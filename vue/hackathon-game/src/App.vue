b4209fad0f93a448875551df10116b8637d07005
claygilk <claygilkerson@gmail.com>
2021-08-27 16:26:56 -0400

    starting stylings

--- vue/hackathon-game/src/App.vue
+++ vue/hackathon-game/src/App.vue
@@ -12,4 +12,19 @@
 
 <style lang="scss">
 
+#nav{
+  background: #202a25;
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 75px;
+  margin-bottom: 75px;
+}
+
+a:visited{
+  color: white;
+}
+
 </style>
