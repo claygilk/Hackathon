5ddddcb5f9cc5c2247cdb3edde6a0159cb9f9c1b
Jamie <janetzkej@gmail.com>
2021-08-29 15:03:12 -0400

    almost done with HP

--- vue/hackathon-game/src/views/Home.vue
+++ vue/hackathon-game/src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="homepage">
       <h1>Welcome to Scrobble</h1>
       <div>
-      <h1>Gameplay</h1>
+      <h1 class="gameplay">Gameplay</h1>
       <div class="rules">
       <h3>The objective of Scrobble is to spell as many words as you can. Each letter tile has a point value assigned to it, like in Scrabble. The more words you spell, the more points you earn.</h3>
       <h3>To spell a word, click-and-drag a letter tile from your hand to a square on the board. You can only place a tile on an empty square that is vertically or horizontally adjacent to an already placed letter tile. When you start the game, 5 random letter tiles will be placed on the board to get you started.</h3>
@@ -11,15 +11,15 @@
       <h3>If you get stuck, and can't spell anymore words you can click the "Get New Hand" button, to return your tiles to the deck and draw 10 more random tiles. Drawing a new hand consume a life, and you only get 3 lives per game. When you run out of lives you can no longer refresh you hand.</h3>
       <h3>When you think you can no longer spell anymore words, press the "Give Up" button. This will end the game and prompt you to enter your initals for a chance to make it onto the leaderboards.</h3>
       </div>
-      <h1>
+      <h1 class="development">
         Development
       </h1>
-      <p>
+      <h3>
         "what we used to make the game"
-      </p>
+      </h3>
       </div>
       <div>
-      <h1>
+      <h1 class="meet-the-engineers">
         Meet the Engineers
       </h1>
       <div class="profile">
@@ -66,7 +66,7 @@ div.homepage{
   border-left: 3px solid #34A3CC;
   border-right: 3px solid #34A3CC;
   width: 75vw;
-  height: 100vw;
+  height: auto;
   text-align: center;
 }
 
@@ -87,10 +87,23 @@ div.rules>h3{
   }
 }
 
+.gameplay{
+  color: #ffc438;
+}
+
+.development{
+  color: #ffc438;
+}
+
+.meet-the-engineers{
+  color: #ffc438;
+}
+
 .jamie-pic{
   width: 250px;
   height: auto;
   border-radius: 50%;
+  //box-shadow: 0px 0px 4px 4px #ffc33863;
 }
 .jamiep, .clayp{
   width: 15vw;
@@ -111,6 +124,8 @@ div.profile>div{
   width: 250px;
   height: auto;
   border-radius: 50%;
+  //box-shadow: 0px 0px 4px 4px #33a3cc;
+  //border: 1px solid #33a3cc;
 }
 
 .social-links{
