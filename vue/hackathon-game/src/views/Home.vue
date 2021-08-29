87de92347026a7f6ab04bc26aac72a5e16e7078a
Jamie <janetzkej@gmail.com>
2021-08-29 14:40:33 -0400

    fixing HP profiles

--- vue/hackathon-game/src/views/Home.vue
+++ vue/hackathon-game/src/views/Home.vue
@@ -5,11 +5,11 @@
       <div>
       <h1>Gameplay</h1>
       <div class="rules">
-      <h3>1. The objective of Scrobble is to spell as many words as you can. Each letter tile has a point value assigned to it, like in Scrabble. The more words you spell, the more points you earn.</h3>
-      <h3>2. To spell a word, click-and-drag a letter tile from your hand to a square on the board. You can only place a tile on an empty square that is vertically or horizontally adjacent to an already placed letter tile. When you start the game, 5 random letter tiles will be placed on the board to get you started.</h3>
-      <h3>3. There are some restrictions with spelling words: You can only spell words from left-to-right, or top-to-bottom. The minimum length for a word is 3 letters.</h3>
-      <h3>4. If you get stuck, and can't spell anymore words you can click the "Get New Hand" button, to return your tiles to the deck and draw 10 more random tiles. Drawing a new hand consume a life, and you only get 3 lives per game. When you run out of lives you can no longer refresh you hand.</h3>
-      <h3>5. When you think you can no longer spell anymore words, press the "Give Up" button. This will end the game and prompt you to enter your initals for a chance to make it onto the leaderboards.</h3>
+      <h3>The objective of Scrobble is to spell as many words as you can. Each letter tile has a point value assigned to it, like in Scrabble. The more words you spell, the more points you earn.</h3>
+      <h3>To spell a word, click-and-drag a letter tile from your hand to a square on the board. You can only place a tile on an empty square that is vertically or horizontally adjacent to an already placed letter tile. When you start the game, 5 random letter tiles will be placed on the board to get you started.</h3>
+      <h3>There are some restrictions with spelling words: You can only spell words from left-to-right, or top-to-bottom. The minimum length for a word is 3 letters.</h3>
+      <h3>If you get stuck, and can't spell anymore words you can click the "Get New Hand" button, to return your tiles to the deck and draw 10 more random tiles. Drawing a new hand consume a life, and you only get 3 lives per game. When you run out of lives you can no longer refresh you hand.</h3>
+      <h3>When you think you can no longer spell anymore words, press the "Give Up" button. This will end the game and prompt you to enter your initals for a chance to make it onto the leaderboards.</h3>
       </div>
       <h1>
         Development
@@ -76,27 +76,41 @@ div.homepage{
   justify-content: space-between;
 }
 
-// .rules{
-//   flex-shrink: 6;
-// }
+.rules{
+  margin: 0 150px 0 150px;
+  text-align: justify;
+}
+
+div.rules>h3{
+  &:hover{
+    color: #ffc438;
+  }
+}
 
 .jamie-pic{
   width: 250px;
   height: auto;
   border-radius: 50%;
 }
-.jamiep{
+.jamiep, .clayp{
+  width: 15vw;
   font-size: 1rem;
+  margin: 0 75px 0 75px;
+  text-align: justify;
+  margin-top: 20px;
+}
+
+div.profile>div{
+  width: 35vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .clay-pic{
   width: 250px;
   height: auto;
   border-radius: 50%;
-}
-
-.clayp{
-  font-size: 1rem;
 }
 
 .social-links{
