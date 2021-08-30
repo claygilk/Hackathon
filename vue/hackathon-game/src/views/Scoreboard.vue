04a46c532045f56d6119238cf3d229af8e2156ec
Jamie <janetzkej@gmail.com>
2021-08-30 11:48:58 -0400

    added footer

--- vue/hackathon-game/src/views/Scoreboard.vue
+++ vue/hackathon-game/src/views/Scoreboard.vue
@@ -4,6 +4,18 @@
     <h1>Leaderboards</h1>
     <Scores/>
   </div>
+  <footer>
+
+      <p>Copyright © Scrobble 2021</p>
+      <!-- <div>
+        <ul class="footer-links">
+          <li><i class="fab fa-linkedin"></i><a href="https://www.linkedin.com"></a></li>
+          <li><i class="fab fa-github-square"></i><a href="https://github.com"></a></li>
+          <li><i class="fab fa-twitter"></i><a href="https://twitter.com"></a></li>
+          <li><i class="fab fa-instagram-square"></i><a href="https://www.instagram.com"></a></li>
+        </ul>
+      </div> -->
+    </footer>
 </div>
 </template>
 
